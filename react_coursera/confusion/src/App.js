edf1c982682b0681c12bc93c1036661a5f27e245
haaaziq <haziqkhan123@gmail.com>
2021-07-18 18:53:38 +0530

    React Components part 2

--- react_coursera/confusion/src/App.js
+++ react_coursera/confusion/src/App.js
@@ -2,18 +2,29 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/menuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="dark">
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar dark color="dark">
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
