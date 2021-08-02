fe246c59a26114e288a72d36be6e4d2a7b8ce214
haaaziq <haziqkhan123@gmail.com>
2021-08-03 01:45:57 +0530

    Added routes using React Router

--- react_coursera/confusion/src/App.js
+++ react_coursera/confusion/src/App.js
@@ -1,11 +1,14 @@
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- react_coursera/confusion/src/components/MainComponent.js
+++ react_coursera/confusion/src/components/MainComponent.js
@@ -6,26 +6,35 @@ import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 class Main extends Component {
+  
   constructor(props){
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     }
   }
 
-    onDishSelect(dishID){
-        this.setState({selectedDish:dishID});
-    }
 
   render(){
+
+    const Homepage= () => {
+      return(
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)} />
-        <DishDetail  dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+          <Switch>
+            <Route path='/home' component={Homepage} />
+            <Route path='/menu' component={()=> <Menu dishes={this.state.dishes} />} />
+            <Redirect to='/home'/>
+          </Switch>
         <Footer />
       </div>
     );
