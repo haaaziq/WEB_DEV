5dd62c3a9bfa3987dd46fb3ee27e5fe6ead81341
haaaziq <haziqkhan123@gmail.com>
2021-08-06 22:40:59 +0530

    SPA part I

--- react_coursera/confusion/src/components/MainComponent.js
+++ react_coursera/confusion/src/components/MainComponent.js
@@ -2,12 +2,17 @@
 
 import { Component } from 'react';
 import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
-import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent'
+// import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Route, Switch, Redirect } from 'react-router-dom';
+
+import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 
 class Main extends Component {
   
@@ -15,6 +20,9 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      leaders: LEADERS,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
     }
   }
 
@@ -23,7 +31,11 @@ class Main extends Component {
 
     const Homepage= () => {
       return(
-        <Home />
+        <Home
+          dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+          leader={this.state.leaders.filter((leader)=> leader.featured)[0]} 
+        />
       );
     }
 
@@ -32,7 +44,8 @@ class Main extends Component {
         <Header />
           <Switch>
             <Route path='/home' component={Homepage} />
-            <Route path='/menu' component={()=> <Menu dishes={this.state.dishes} />} />
+            <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes} />} />
+            <Route exact path='/contactus' component={Contact} />
             <Redirect to='/home'/>
           </Switch>
         <Footer />
