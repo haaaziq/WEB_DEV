7706521658ea80bd727d4360759d4b917563f51a
haaaziq <haziqkhan123@gmail.com>
2021-08-19 02:40:22 +0530

    Submit Comment Form Added - Assignment Week 3

--- react_coursera/confusion/src/components/DishdetailComponent.js
+++ react_coursera/confusion/src/components/DishdetailComponent.js
@@ -1,6 +1,85 @@
-import React from 'react';
-import { Card, CardImg, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { Component } from 'react';
+import { Card, CardImg, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem, Label, Row, Modal, ModalBody, ModalHeader, Button } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
+
+const minLength = (len) => (val) => val && (val.length >= len);
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+
+class CommentForm extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isCommentModalOpen: false,
+        };
+
+        this.toggleCommentModal = this.toggleCommentModal.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+
+    toggleCommentModal(){
+        this.setState({
+            isCommentModalOpen : !this.state.isCommentModalOpen,
+        });
+    }
+
+    handleCommentSubmit(values){
+        this.toggleCommentModal();
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Button outline onClick={this.toggleCommentModal} ><span className='fa fa-pencil fa-lg'></span> Submit Comment</Button>
+                
+                <Modal isOpen={this.state.isCommentModalOpen} toggle={this.toggleCommentModal}>
+                    <ModalHeader toggle={this.toggleCommentModal}><b>Submit Comment</b></ModalHeader>
+                    <ModalBody className='mx-3'>
+                        <LocalForm onSubmit={(values) => this.handleCommentSubmit(values)} >
+                            <Row className='form-group'>
+                                <Label htmlFor='rating'>Rating</Label>
+                                <Control.select model='.rating' name='rating' id='rating' className='form-control'>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className='form-group'>
+                                <Label htmlFor='author'>Your Name</Label>
+                                <Control.text model='.author' name='author' id='author' placeholder='Your Name' className='form-control' 
+                                    validators={{
+                                        minLength: minLength(3), maxLength: maxLength(15)
+                                    }}
+                                />
+                                <Errors 
+                                    className='text-danger'
+                                    model='.author'
+                                    show='touched'
+                                    messages={{
+                                        minLength: "Must be greater than 2 characters",
+                                        maxLength: "Must be 15 characters or less",
+                                    }}
+                                />
+                            </Row>
+                            <Row className='form-group'>
+                                <Label htmlFor='comment'>Comment</Label>
+                                <Control.textarea model='.comment' name='comment' id='comment' rows='6' className='form-control' />
+                            </Row>
+                            <Row className='form-group'>
+                                <Button type='submit' color='primary'>Submit</Button>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+        );
+    }
+}
 
     function DateFormat({date}){
         const format = {year: 'numeric', month: 'short', day: 'numeric'};
@@ -25,6 +104,7 @@ import { Link } from 'react-router-dom';
                     <ul className='list-unstyled'>
                         {allComments}
                     </ul>
+                    <CommentForm />
                 </div>
             );
 
@@ -70,7 +150,7 @@ function DishDetail(props){
                 <hr />
             </div>
             <div className='row'>
-                <RenderDish dish={props.dish}/>
+                <RenderDish dish={props.dish} />
                 <RenderComments comments={props.comments} />
             </div>
         </div>
