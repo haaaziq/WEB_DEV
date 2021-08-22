da62c45cf4fb799512d6d53e3eb4ab3069b38fff
haaaziq <haziqkhan123@gmail.com>
2021-08-22 23:24:54 +0530

    React Redux Form revisited - storing form state in Redux Store

--- react_coursera/confusion/src/components/ContactComponent.js
+++ react_coursera/confusion/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Breadcrumb, BreadcrumbItem, Row, Label, Col,  Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Form, Control, Errors, actions } from 'react-redux-form';
 
 // Validator Functions
 const required = (val) => val && val.length;
@@ -21,6 +21,7 @@ class Contact extends Component{
     handleSubmit(values){
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     render(){
@@ -71,7 +72,7 @@ class Contact extends Component{
                         <h3>Send us Your Feedback</h3>
                     </div>
                     <div className='col-12 col-md-9'>
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model='feedback' onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className='form-group'>
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -178,7 +179,7 @@ class Contact extends Component{
                                     <Button type="submit" color="primary">Send Feedback</Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
--- react_coursera/confusion/src/components/MainComponent.js
+++ react_coursera/confusion/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import About from './AboutComponent';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapStateToProps = (state) => {
   return{
@@ -25,6 +26,7 @@ const mapDispatchToProps = (dispatch) => ({
   // dispatching the action to reducer...to reflect change in state
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
   fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -69,7 +71,7 @@ class Main extends Component {
             <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
             <Route exact path='/menu' component={()=> <Menu dishes={this.props.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithID} />
-            <Route exact path='/contactus' component={Contact} />
+            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
             <Redirect to='/home'/>
           </Switch>
         <Footer />
