b16b5956a0659a9d704263378819ed45226e7d7a
haaaziq <haziqkhan123@gmail.com>
2021-08-27 01:52:13 +0530

    Handled errors - in Fetch by handling Promises- throw/catch

--- react_coursera/confusion/src/redux/ActionCreators.js
+++ react_coursera/confusion/src/redux/ActionCreators.js
@@ -9,8 +9,21 @@ import { baseUrl } from "../shared/baseUrl";
         dispatch(dishesLoading(true));
 
         return fetch(baseUrl + 'dishes')
+            .then(response => {
+                if(response.ok)
+                    return response;
+                else{ // occur when there is response from the Server which contains error (means, app is able to communicate with the server)
+                    var error = new Error('Error! ' + response.status + ' ' + response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+            }, error => { // occur when there is no response from the server, maybe when server is not running
+                var errorMsg = new Error(error.message);
+                throw errorMsg;
+            })
             .then(response => response.json())
-            .then(dishes => dispatch(addDishes(dishes)));
+            .then(dishes => dispatch(addDishes(dishes)))
+            .catch(error => dispatch(dishesFailed(error.message))); // in catch block we'll cath above error
     }
 
     // addDishes, dishesLoading, dishesFailed - Action Creators, returns Action Object
@@ -45,8 +58,21 @@ import { baseUrl } from "../shared/baseUrl";
     // Thunk for fetching comments
     export const fetchComments = () => (dispatch) => {
         return fetch(baseUrl + 'comments')
+            .then(response => {
+                if(response.ok)
+                    return response;
+                else{
+                    var error = new Error('Error! ' + response.status + ' ' + response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+            }, error => {
+                var errorMsg = new Error(error.message);
+                throw errorMsg;
+            })
             .then(response => response.json())
-            .then(comments => dispatch(addComments(comments)));
+            .then(comments => dispatch(addComments(comments)))
+            .catch(error => dispatch(commentsFailed(error.message)));
     }
 
     export const addComments = (comments) => ({
@@ -67,8 +93,21 @@ import { baseUrl } from "../shared/baseUrl";
         dispatch(promotionsLoading(true));
 
         return fetch(baseUrl + 'promotions')
+            .then(response => {
+                if(response.ok)
+                    return response;
+                else{
+                    var error = new Error('Error! ' + response.status + ' ' + response.statusText);
+                    error.response = response;
+                    throw error;
+                }
+            }, error => {
+                var errorMsg = new Error(error.message);
+                throw errorMsg;
+            })
             .then(response => response.json())
-            .then(promotions => dispatch(addPromotions(promotions)));
+            .then(promotions => dispatch(addPromotions(promotions)))
+            .catch(error => dispatch(promotionsFailed(error.message)));
     }
 
     export const addPromotions = (promos) => ({
